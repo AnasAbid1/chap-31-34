564c17ab89e9440798f7b67f7212247dcb406c2a
Anas Abid <abid.anas3@gmail.com>
2021-11-14 12:19:17 -0800

    practise

---
+++ app.js
@@ -0,0 +1,102 @@
+// alert("HELLO CLASS")
+
+
+// //1)
+// var date = new Date ();
+// document.write(date)
+
+// //2)
+// var monthNames = ["January", "February", "March", "April", "May", "June",
+//   "July", "August", "September", "October", "November", "December"];
+
+// var d = new Date();
+// document.write("The current month is " + monthNames[d.getMonth()]);
+// document.write("The current month is " + d.getDay());
+
+
+//3)
+// var days = ["sunday", "monday", "tuesday","wednesday","thursday","friday","Saturday"]
+// var d = new Date()
+// console.log(days[d.getDay()])
+// console.log(days[0])
+
+//4)
+// var days = ["sunday", "monday", "tuesday","wednesday","thursday","friday","Saturday"]
+// var d = new Date()
+// console.log(days[d.getDay()])
+// if(days==0,5){
+//     document.write("ITS A FUN DAY")
+// }
+
+//5)
+// var d1 = new Date();
+// var d2 =d1.getDate()
+// console.log(d2)
+// if(d2<=15){
+//     document.write("FIRST FIFTEEN DAYS OF A MONTH ")
+// }else{document.write("LAST FIFTEEN DAYS OF A MONTH ")}
+
+//6)
+// var currentDate = new Date("01/01/1970 12:00:00")
+// var milliSec = currentDate.getTime()
+// console.log(milliSec)
+// var month = 11; // January
+// var d = new Date(2020, month + 1,0);
+// console.log(d.toString()); // last day in January
+
+// var currentDate = new Date("01/01/1970 12:00:00")
+// var minutes = currentDate.getMinutes()
+// console.log(minutes)
+// var month = 11; // January
+// var d = new Date(2020, month + 1,0);
+// console.log(d.toString()); // last day in January
+
+
+//7)
+// var currentTime = new Date()
+// let currentHours =currentHours.getHours()
+// if(currentHours<=12){
+//     document.write("It's P.M.")
+// }else{document.write("It's A.M.")}
+
+//8)
+// var month = 0; // January
+// var d = new Date(2008, month + 1, 0);
+// console.log(d.toString());
+
+//9)
+// var dateNow = new Date().getDay
+// var lastDate =("2015-03-25")
+// var daysPassed = dateNow - lastDate
+// console.log(daysPassed)
+
+
+
+
+
+//10)
+
+
+
+
+
+//11)
+
+
+
+
+//12)
+
+
+
+
+
+
+//13)
+// var age = +prompt("PLEASE ENTER YOUR AGE !!!")
+// var currentYear = new Date().getFullYear();
+// console.log(currentYear)
+
+// var birthYear = currentYear - age
+// document.write("YOUR BIRTH YEAR : "+birthYear) 
+
